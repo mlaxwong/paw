77eb0632e403f17aa30b07d9ed8c9185a0e24dfe
mlaxwong <mlaxwong@gmail.com>
2019-09-03 01:12:38 +0800

    added numabbr

--- src/helpers/StringHelper.php
+++ src/helpers/StringHelper.php
@@ -8,22 +8,24 @@ class StringHelper extends \yii\helpers\StringHelper
         preg_match_all('/\{([^\}]+)\}/i', $message, $matchs);
         $matchs = array_combine($matchs[0], $matchs[1]);
 
-        if (!$matchs) return $message;
+        if (!$matchs) {
+            return $message;
+        }
 
         $placeholders = [];
-        foreach ($matchs as $fullMatch => $match)
-        {
+        foreach ($matchs as $fullMatch => $match) {
             $extraString = '';
             preg_match('/(.+)\|(.+)/i', $match, $extraMatchs);
-            if ($extraMatchs) list($fullExtraMatch, $match, $extraString) = $extraMatchs;
+            if ($extraMatchs) {
+                list($fullExtraMatch, $match, $extraString) = $extraMatchs;
+            }
 
             $parts = explode('.', $match);
             $pointer = $params;
             $notFound = false;
             $defaultValue = $makeItEmpty ? '' : $fullMatch;
 
-            foreach ($parts as $part)
-            {
+            foreach ($parts as $part) {
                 if (isset($pointer->{$part})) {
                     $pointer = $pointer->{$part};
                 } else if (isset($pointer[$part])) {
@@ -38,5 +40,21 @@ class StringHelper extends \yii\helpers\StringHelper
         }
 
         return ($placeholders === []) ? $message : strtr($message, $placeholders);
+    }
+
+    public function numabbr($number)
+    {
+        $abbrevs = [12 => 'T', 9 => 'B', 6 => 'M', 3 => 'K', 0 => ''];
+
+        foreach ($abbrevs as $exponent => $abbrev) {
+            if (abs($number) >= pow(10, $exponent)) {
+                $display = $number / pow(10, $exponent);
+                $decimals = ($exponent >= 3 && round($display) < 100) ? 1 : 0;
+                $number = number_format($display, $decimals) . $abbrev;
+                break;
+            }
+        }
+
+        return $number;
     }
 }
